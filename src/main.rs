23bdc447d086f88461fcbe604290bac0d2cbc826
Auri Gabriel <ririmello.a@gmail.com>
2022-08-19 00:52:05 -0300

    more code

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,37 @@
 use bevy::prelude::*;
+use player::PlayerPlugin;
+
+mod components;
+mod player;
 
 // region: -- Assests Constant
 
 const PLAYER_SPRITE: &str = "player_a_01.png";
 const PLAYER_SIZE: (f32, f32) = (144., 75.);
 
+const SPRITE_SCALE: f32 = 0.5;
+
 // endregion: -- Assests Constant
+
+// region -- Game Constants
+
+const TIME_STEP: f32 = 1. / 60.;
+const BASE_SPEED: f32 = 500.;
+
+// endregion -- Game Constants
+
+// region -- Resources
+
+pub struct WinSize {
+    pub w: f32,
+    pub h: f32,
+}
+
+struct GameTextures {
+    player: Handle<Image>,
+}
+
+// endregion -- Resources
 
 fn main() {
     App::new()
@@ -17,6 +43,7 @@ fn main() {
             ..Default::default()
         })
         .add_plugins(DefaultPlugins)
+        .add_plugin(PlayerPlugin)
         .add_startup_system(setup_system)
         .run();
 }
@@ -33,12 +60,13 @@ fn setup_system(
     let window = windows.get_primary_mut().unwrap();
     let (win_w, win_h) = (window.width(), window.height());
 
-    // position window
-    window.set_position(IVec2::new(2780, 4900));
+    //add winsize resource
+    let win_size = WinSize { w: win_w, h: win_h };
+    commands.insert_resource(win_size);
 
-    // add player
-    commands.spawn_bundle(SpriteBundle {
-        texture: asset_server.load(PLAYER_SPRITE),
-        ..Default::default()
-    });
+    //add GameTexture resource
+    let game_textures = GameTextures {
+        player: asset_server.load(PLAYER_SPRITE),
+    };
+    commands.insert_resource(game_textures);
 }
