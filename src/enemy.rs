58474b1ae5229045b1786fc86ed0f89f49879061
Auri Gabriel <ririmello.a@gmail.com>
2022-08-19 05:06:12 -0300

    enemy fires back wow
    very effective!!

--- src/enemy.rs
+++ src/enemy.rs
@@ -1,8 +1,10 @@
+use std::f32::consts::PI;
+
 use crate::{
-    components::{Enemy, SpriteSize},
-    EnemyCount, GameTextures, WinSize, ENEMY_MAX, ENEMY_SIZE, SPRITE_SCALE,
+    components::{Enemy, FromEnemy, Laser, Movable, SpriteSize, Velocity},
+    EnemyCount, GameTextures, WinSize, ENEMY_LASER_SIZE, ENEMY_MAX, ENEMY_SIZE, SPRITE_SCALE,
 };
-use bevy::{prelude::*, time::FixedTimestep};
+use bevy::{ecs::schedule::ShouldRun, prelude::*, time::FixedTimestep};
 use rand::{thread_rng, Rng};
 
 pub struct EnemyPlugin;
@@ -13,6 +15,11 @@ impl Plugin for EnemyPlugin {
             SystemSet::new()
                 .with_run_criteria(FixedTimestep::step(1.))
                 .with_system(enemy_spawn_system),
+        )
+        .add_system_set(
+            SystemSet::new()
+                .with_run_criteria(enemy_fire_criteria)
+                .with_system(enemy_fire_system),
         );
     }
 }
@@ -44,5 +51,40 @@ fn enemy_spawn_system(
             .insert(Enemy)
             .insert(SpriteSize::from(ENEMY_SIZE));
         enemy_count.0 += 1;
+    }
+}
+
+fn enemy_fire_criteria() -> ShouldRun {
+    if thread_rng().gen_bool(1. / 60.) {
+        ShouldRun::Yes
+    } else {
+        ShouldRun::No
+    }
+}
+
+fn enemy_fire_system(
+    mut commands: Commands,
+    game_texures: Res<GameTextures>,
+    enemy_query: Query<&Transform, With<Enemy>>,
+) {
+    for &tf in enemy_query.iter() {
+        let (x, y) = (tf.translation.x, tf.translation.y);
+        //spawn enemy laser sprite
+        commands
+            .spawn_bundle(SpriteBundle {
+                texture: game_texures.enemy_laser.clone(),
+                transform: Transform {
+                    translation: Vec3::new(x, y - 15., 0.),
+                    rotation: Quat::from_rotation_x(PI),
+                    scale: Vec3::new(SPRITE_SCALE, SPRITE_SCALE, 1.),
+                    ..Default::default()
+                },
+                ..Default::default()
+            })
+            .insert(Laser)
+            .insert(SpriteSize::from(ENEMY_LASER_SIZE))
+            .insert(FromEnemy)
+            .insert(Movable { auto_despawn: true })
+            .insert(Velocity { x: 0., y: -1. });
     }
 }
