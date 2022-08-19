d2193d550e92279efb33b02dcc5808176610f0d5
Auri Gabriel <ririmello.a@gmail.com>
2022-08-19 00:56:50 -0300

    keyboard controls

--- src/player.rs
+++ src/player.rs
@@ -10,7 +10,8 @@ pub struct PlayerPlugin;
 impl Plugin for PlayerPlugin {
     fn build(&self, app: &mut App) {
         app.add_startup_system_to_stage(StartupStage::PostStartup, player_spawn_system)
-            .add_system(player_movement_system);
+            .add_system(player_movement_system)
+            .add_system(player_keyboard_event_system);
     }
 }
 
@@ -33,6 +34,21 @@ fn player_spawn_system(
         })
         .insert(Player)
         .insert(Velocity { x: 0., y: 0. });
+}
+
+fn player_keyboard_event_system(
+    kb: Res<Input<KeyCode>>,
+    mut query: Query<&mut Velocity, With<Player>>,
+) {
+    if let Ok(mut velocity) = query.get_single_mut() {
+        velocity.x = if kb.pressed(KeyCode::Left) {
+            -1.
+        } else if kb.pressed(KeyCode::Right) {
+            1.
+        } else {
+            0.
+        }
+    }
 }
 
 fn player_movement_system(mut query: Query<(&Velocity, &mut Transform), With<Player>>) {
